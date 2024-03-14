9e7e35caef9f4e582328070d50119f1b7c3e13a4
Eban Eban <ebaneshar@gmail.com>
2024-03-15 02:34:25 +0530

    using package manager console added migration file for Employee Table and updated database and created employee table

---
+++ AspNetMvcGrud.DataAccessLayer/Migrations/20240314205932_CreatedEmployeeTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetMvcGrud.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedEmployeeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    ContactNumber = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    DateOfBirth = table.Column<DateOnly>(type: "date", nullable: false),
+                    Education = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Occupation = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    JoinedDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    RelievedDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
