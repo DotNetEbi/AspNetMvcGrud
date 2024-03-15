b00ec961dc0b5c0d490499a8a8de891cf1686b72
Eban Eban <ebaneshar@gmail.com>
2024-03-15 10:56:32 +0530

    added validator project and added FluentValidation package and added employee validator class

---
+++ AspNetMvcGrud.Validator/Employee/EmployeeValidator.cs
@@ -0,0 +1,45 @@
+﻿using AspNetMvcGrud.Domain;
+using FluentValidation;
+
+namespace AspNetMvcGrud.Validator;
+
+public class EmployeeValidator :AbstractValidator<Employee>
+{
+    public EmployeeValidator()
+    {
+        RuleFor(employee => employee.Name)
+            .NotEmpty()
+            .WithMessage("Name Required");
+
+		RuleFor(employee => employee.Gender)
+			.NotEmpty()
+			.WithMessage("Gender Required");
+
+		RuleFor(employee => employee.Address)
+			.NotEmpty()
+			.WithMessage("Address Required");
+
+		RuleFor(employee => employee.ContactNumber)
+			.NotEmpty()
+			.WithMessage("Contact Number Required")
+			.DependentRules(() =>
+			{
+				RuleFor(employee => employee.ContactNumber.Length)
+					.GreaterThanOrEqualTo(10)
+					.OverridePropertyName(nameof(Employee.ContactNumber))
+					.WithMessage("Contact Number Invalid");
+			});
+
+		RuleFor(employee => employee.DateOfBirth)
+			.NotEmpty()
+			.WithMessage("Date Of Birth Required");
+
+		RuleFor(employee => employee.Education)
+			.NotEmpty()
+			.WithMessage("Education Required");
+
+		RuleFor(employee => employee.JoinedDate)
+			.NotEmpty()
+			.WithMessage("Joined Date Required");
+	}
+}
